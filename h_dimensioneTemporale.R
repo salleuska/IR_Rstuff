59c15c85e05bfe489858711e214acfcea4668f36
Alan Giacomin <alan.giacomin@gmail.com>
2014-03-26 07:36:10 +0100

    aggiunto tempo elaborazione

--- h_dimensioneTemporale.R
+++ h_dimensioneTemporale.R
@@ -1,18 +1,52 @@
 # Prova con i dati del 1992
-dim.temp<- read.delim("~/Scrivania/heidel_dimensioneTemporale (id+date).txt", header=F)
+#dim.temp<- read.delim("~/Scrivania/heidel_dimensioneTemporale (id+date).txt", header=F)
+
+tic <- function(gcFirst = TRUE, type=c("elapsed", "user.self", "sys.self"))
+{
+  type <- match.arg(type)
+  assign(".type", type, envir=baseenv())
+  if(gcFirst) gc(FALSE)
+  tic <- proc.time()[type]         
+  assign(".tic", tic, envir=baseenv())
+  invisible(tic)
+}
+
+toc <- function()
+{
+  type <- get(".type", envir=baseenv())
+  toc <- proc.time()[type]
+  tic <- get(".tic", envir=baseenv())
+  print(toc - tic)
+  invisible(toc)
+}
+
+
+
+
+tic()
+dim.temp<- read.delim("~/Documents/GIT/Rstuff/heidel_dimensioneTemporale.txt", header=F)
+toc()
 colnames(dim.temp) <- c("id", "day")
 # Gestion date in R-base
+tic()
 dim.temp$day <- as.Date(dim.temp$day)
+toc()
+tic()
 str(dim.temp)
 summary(dim.temp$day)
+toc()
 hist(dim.temp$day, breaks = "year")
 
 # Numero di documenti
+tic()
 length(unique(dim.temp$id))
+toc()
 
 # Prova con primo documento
+tic()
 prova <- dim.temp[which(dim.temp$id ==levels(dim.temp$id)[1]), ]
 prova <- droplevels(prova)
+toc()
 summary(prova$day)
 hist(prova$day, breaks = "months")
 
