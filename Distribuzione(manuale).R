e77cdcfb0a86a88ba1c033f36f6ecd25fc581aa3
salleuska <sallypaganin@gmail.com>
2014-04-08 12:47:04 +0200

    aggiunte a Distribuzione(manuale)

--- Distribuzione(manuale).R
+++ Distribuzione(manuale).R
@@ -17,7 +17,6 @@ colnames(dim.temp) <- c("id", "day")
 str(dim.temp)
 dim.temp$day <- as.Date(dim.temp$day)
 
-
 # Prova "a mano"
 # considero la distribuzione discreta empirica
 
@@ -28,7 +27,11 @@ tb <- table(prova$day)/length(prova$day)
 plot(tb, type = "h")
 plot(sort(tb, decreasing= T), type = "h", ylim = c(0, max(sort(tb))))
 
-hdr <- function(prob, data)
+# La seguente funzione:
+# calcola le probabilità empiriche (frequenze/tot) e le ordina in maniera descrescente;
+# le somma fino a quando non viene raggiunta la proporzione di densità di probabilità
+# voluta, e restituisce la lista delle date associate 
+hdr0 <- function(prob = 0.95, data)
 {
   freq <- table(data$day)/length(data$day)
   freq.ord <- sort(freq, decreasing= T)
@@ -44,8 +47,54 @@ hdr <- function(prob, data)
 }
 
 startTimer()
-test <- hdr(0.9, data = prova)
+test <- hdr0(0.7, data = prova)
 stopTimer()
 
+plot(as.Date(test$date), type = "p")
+
 summary(as.Date(test$date))
-ndays <- as.integer(max(as.Date(test$date)) - min(as.Date(test$date)))
+max(as.Date(test$date)) - min(as.Date(test$date))
+
+
+#-------------------------------------------------------------#
+# modifiche per applicazione
+hdr <- function(prob = 0.9, data)
+{
+  out <- numeric(3)
+  freq <- table(data$day)/length(data$day)
+  freq.ord <- sort(freq, decreasing= T)
+  cum.freq <- as.numeric(freq.ord[1])
+  date <- as.Date(names(freq.ord)[1])
+  while(sum(cum.freq) < prob)
+  {
+    i <- length(cum.freq)
+    cum.freq <- c(cum.freq, freq.ord[i+ 1])
+    date <- c(date, as.Date(names(freq.ord)[i +1]))
+  }
+  out <- c(sum(cum.freq), as.numeric(max(date) - min(date)))
+  
+}
+test <- hdr(0.9, data = prova)
+
+str(test)
+
+library(plyr)
+startTimer()
+test <- daply(droplevels(dim.temp[1:40000, ]), "id", function(x) hdr(data = x))
+stopTimer()
+
+as.data.frame(test)
+
+# NOTA: bisogna identificare i possibili buchi.. pper ora l'intervallo restituito è solo la
+# differenza tra minimo è massimo.. Potrebbe bastare tenere traccia degli indici..
+
+# Eventualmente 
+library(doParallel)
+
+# registerDoParallel()
+# si può scegliere il numero di copie di R che girano in parallelo con makeCluster
+cl <- makeCluster(4)
+registerDoParallel(cl)
+
+prova <- dim.temp[which(dim.temp$id ==levels(dim.temp$id)[13]), ]
+prova <- droplevels(prova)
